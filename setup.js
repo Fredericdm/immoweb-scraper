dd3da09fc27a6e2a0145edab4721cc263f7f98e4
Fredericdm <35852756+Fredericdm@users.noreply.github.com>
2024-06-09 07:41:24 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 
-// Function to run npm install
+// Function to run npm install in the immoweb-main directory
 function installDependencies() {
   return new Promise((resolve, reject) => {
-    exec('npm install', (error, stdout, stderr) => {
+    const homeDir = os.homedir();
+    const immowebMainPath = path.join(homeDir, 'Desktop', 'immoweb-main');
+    
+    exec('npm install', { cwd: immowebMainPath }, (error, stdout, stderr) => {
       if (error) {
         console.error(`Error during npm install: ${error}`);
         reject(error);
@@ -18,24 +21,24 @@ function installDependencies() {
   });
 }
 
-// Function to create an empty subfolder named 'jsons' inside 'immoweb' folder on the user's desktop
+// Function to create an empty subfolder named 'jsons' inside 'immoweb-main' folder on the user's desktop
 function createJsonsFolder() {
   const homeDir = os.homedir();
-  const immowebPath = path.join(homeDir, 'Desktop', 'immoweb');
-  const folderPath = path.join(immowebPath, 'jsons');
+  const immowebMainPath = path.join(homeDir, 'Desktop', 'immoweb-main');
+  const folderPath = path.join(immowebMainPath, 'jsons');
 
-  if (!fs.existsSync(immowebPath)) {
-    fs.mkdirSync(immowebPath, { recursive: true });
-    console.log('Created immoweb folder on the Desktop');
+  if (!fs.existsSync(immowebMainPath)) {
+    fs.mkdirSync(immowebMainPath, { recursive: true });
+    console.log('Created immoweb-main folder on the Desktop');
   } else {
-    console.log('immoweb folder already exists on the Desktop');
+    console.log('immoweb-main folder already exists on the Desktop');
   }
 
   if (!fs.existsSync(folderPath)) {
     fs.mkdirSync(folderPath, { recursive: true });
-    console.log('Created jsons folder inside immoweb on the Desktop');
+    console.log('Created jsons folder inside immoweb-main on the Desktop');
   } else {
-    console.log('jsons folder already exists inside immoweb on the Desktop');
+    console.log('jsons folder already exists inside immoweb-main on the Desktop');
   }
 }
 
