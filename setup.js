81c308b903e8444514669d2a4c476dd4a2b1d03d
Fredericdm <35852756+Fredericdm@users.noreply.github.com>
2024-06-09 10:20:14 +0200

    Update setup.js

--- setup.js
+++ setup.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const os = require('os');
 
-// Function to run npm install in the immoweb-main directory
+// Function to run npm install in the immoweb-scraper-main directory
 function installDependencies() {
   return new Promise((resolve, reject) => {
     const homeDir = os.homedir();
-    const immowebMainPath = path.join(homeDir, 'Desktop', 'immoweb-main');
+    const immowebMainPath = path.join(homeDir, 'Desktop', 'immoweb-scraper-main');
     
     exec('npm install', { cwd: immowebMainPath }, (error, stdout, stderr) => {
       if (error) {
@@ -24,21 +24,21 @@ function installDependencies() {
 // Function to create an empty subfolder named 'jsons' inside 'immoweb-main' folder on the user's desktop
 function createJsonsFolder() {
   const homeDir = os.homedir();
-  const immowebMainPath = path.join(homeDir, 'Desktop', 'immoweb-main');
+  const immowebMainPath = path.join(homeDir, 'Desktop', 'immoweb-scraper-main');
   const folderPath = path.join(immowebMainPath, 'jsons');
 
   if (!fs.existsSync(immowebMainPath)) {
     fs.mkdirSync(immowebMainPath, { recursive: true });
-    console.log('Created immoweb-main folder on the Desktop');
+    console.log('Created immoweb-scraper-main folder on the Desktop');
   } else {
-    console.log('immoweb-main folder already exists on the Desktop');
+    console.log('immoweb-scraper-main folder already exists on the Desktop');
   }
 
   if (!fs.existsSync(folderPath)) {
     fs.mkdirSync(folderPath, { recursive: true });
-    console.log('Created jsons folder inside immoweb-main on the Desktop');
+    console.log('Created jsons folder inside immoweb-scraper-main on the Desktop');
   } else {
-    console.log('jsons folder already exists inside immoweb-main on the Desktop');
+    console.log('jsons folder already exists inside immoweb-scraper-main on the Desktop');
   }
 }
 
